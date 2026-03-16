13c0497050e70ae59a1dcd3ebe31cbe86a44356f
psipatrol <s33470@pjwstk.edu.pl>
2026-03-16 17:44:15 +0100

    StatisticsHelper test input value changed

--- apbd-cw1-git-s33470/Program.cs
+++ apbd-cw1-git-s33470/Program.cs
@@ -1,5 +1,5 @@
 ﻿using apbd_cw1_git_s33470;
 
 Console.WriteLine("Hello there!");
-Console.WriteLine(StatisticsHelper.GetTextLenght(StatisticsHelper.GetText("Hello there!")));
+Console.WriteLine(StatisticsHelper.GetTextLenght(StatisticsHelper.GetText("Hello there General Kenobi!")));
 Console.WriteLine(StatisticsHelper.HexToInt("FF"));
