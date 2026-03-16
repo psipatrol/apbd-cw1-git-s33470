4de4f461e909057dbf8e4644d397f41a46da551a
psipatrol <s33470@pjwstk.edu.pl>
2026-03-16 17:07:19 +0100

    StatisticsHelper new GetText feature applied to main class

--- apbd-cw1-git-s33470/Program.cs
+++ apbd-cw1-git-s33470/Program.cs
@@ -1,4 +1,4 @@
 ﻿using apbd_cw1_git_s33470;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine(StatisticsHelper.GetTextLenght("Hello, World!"));
+Console.WriteLine(StatisticsHelper.GetTextLenght(StatisticsHelper.GetText("Hello, World!")));
